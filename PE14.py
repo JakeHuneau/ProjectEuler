40d7c09574b2af0f460c343d4338153646bc7b98
JakeHuneau <jakehuneau@yahoo.com>
2018-09-08 16:44:32 -0400

    PE14

---
+++ PE14.py
@@ -0,0 +1,33 @@
+"""
+Longest collatz sequence of starting number under 1 million.
+
+n -> n/2 (even n)
+n -> 3n + 1 (odd n)
+
+:thoughts:
+Just cache the length of a sequence in a dict so not to repeat stuff
+"""
+
+def next_collatz(n: int) -> int:
+    """Find next number in collatz sequence"""
+    if not n % 2:
+        return n // 2
+    else:
+        return 3 * n + 1
+
+def pe14():
+    collatz_len = dict()  # {num: length of sequence}
+    for i in range(1, 1_000_000):
+        curr = i
+        count = 0
+        while curr != 1:
+            if curr in collatz_len:
+                count += collatz_len[curr]
+                break
+            curr = next_collatz(curr)
+            count += 1
+        collatz_len[i] = count
+    print(max(collatz_len, key=lambda key: collatz_len[key]))
+
+if __name__ == '__main__':
+    pe14()
